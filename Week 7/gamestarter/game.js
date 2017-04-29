abf6536ece7eeed212eac710003c22f2186139dd
camerono <camough@gmail.com>
2017-04-29 11:06:35 -0700

    added basic game start/end handlers, and audio

--- Week 7/gamestarter/game.js
+++ Week 7/gamestarter/game.js
@@ -1,10 +1,14 @@
 var currentScore;
 var livesRemaining;
-var gameLengthSeconds = 30;
+var gameLengthSeconds = 3;
 var timedCounter;
 var gameStarted = false;
 var playfield;
 var context;
+var sounds = {
+    backgroundMusic : null,
+    gameOver : null
+}
 
 // everything in here happens once the HTML document is loaded.
 $(function () {
@@ -13,6 +17,7 @@ $(function () {
    context = playfield.getContext("2d");
    // initialization routine(s)
    initializeListeners();
+   initializeAudio();
 
 });
 
@@ -20,32 +25,47 @@ function initializeListeners(){
   document.onkeydown = keyEventHandler;
 }
 
+function initializeAudio(){
+  sounds.backgroundMusic = new Audio("audio/386412__setuniman__restless-1o69.wav");
+	sounds.backgroundMusic.loop = true;
+	sounds.backgroundMusic.volume = .25;
+	sounds.backgroundMusic.load();
+	sounds.gameOver = new Audio("audio/76376__deleted-user-877451__game-over.wav");
+	sounds.gameOver.load();
+}
+
 function gameStart(){
   gameStarted = true;
-  timedCounter = gameLengthSeconds;
+  $('#gameStart').hide();
+  $('#gameOver').hide();
+
+  sounds.backgroundMusic.play();
   startTimer();
 }
 
 function gameEnd(){
   gameStarted = false;
+  sounds.backgroundMusic.pause();
+	sounds.gameOver.currentTime = 0;
+	sounds.gameOver.play();
+	$('#gameOver').show();
 }
 
 function updateClock( secondsRemaining ){
-  // update on screen clock
+  $('#timerValue').text(secondsRemaining);
 }
 
 function startTimer(){
+  timedCounter = gameLengthSeconds;
+  updateClock(timedCounter);
   var interval = setInterval(function() {
     if (timedCounter >0){
       timedCounter -= 1;
-      updateClock( timedCounter );
-      if (timedCounter == 1) {
-        // gameEndSound.play(); // play sound with 1 sec. remaining
-      }
     } else {
       clearInterval(interval);
       gameEnd();
     }
+    updateClock( timedCounter );
   },1000);
 }
 
