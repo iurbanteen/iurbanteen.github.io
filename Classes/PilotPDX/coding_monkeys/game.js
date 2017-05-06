e9a6320683d50d53ff012be05a21072bd0edcc00
jamesm <themotleycoder@gmail.com>
2017-05-06 11:13:54 -0700

    fixed scene drawing and added collision detection example

--- Classes/PilotPDX/coding_monkeys/game.js
+++ Classes/PilotPDX/coding_monkeys/game.js
@@ -1,14 +1,22 @@
 var currentScore;
 var livesRemaining;
-var gameLengthSeconds = 3;
+var gameLengthSeconds = 10;
 var timedCounter;
 var gameStarted = false;
 var playfield;
 var context;
+var mainCharacter;
+var level1;
+var backgroundXLoc = 0;
+var canvasXLoc = 0;
+var backgroundTimer;
+var keyEvent;
+
 var sounds = {
     backgroundMusic: null,
     gameOver: null
 }
+
 var dude = {
   image : null,
   standing : {x : 5, y : 2, width: 52, height: 96},
@@ -35,91 +43,85 @@ var dude = {
   jumping : { x: 271, y: 2, width: 58, height: 91}
 }
 
-var mainCharacter;
-var level1;
-
 var images = {
     groundImage: null,
     decorationImage: null
 }
-var groundShapes = {
-  1: [0, 0, 100, 100],
-  2: [0, 100, 100, 100],
-  3: [0, 200, 100, 100],
-  4: [0, 300, 100, 100],
-  5: [0, 400, 100, 100],
-  6: [0, 500, 100, 100],
-  7: [0, 600, 100, 100],
-  8: [0, 700, 100, 100],
-  9: [0, 800, 100, 50],
-  10: [0, 850, 100, 50],
-  11: [0, 900, 100, 50],
+var groundImageMap = {
+  1: {x:0, y:0, width:100, height:100},
+  2: {x:0, y:100, width:100, height:100},
+  3: {x:0, y:200, width:100, height:100},
+  4: {x:0, y:300, width:100, height:100},
+  5: {x:0, y:400, width:100, height:100},
+  6: {x:0, y:500, width:100, height:100},
+  7: {x:0, y:600, width:100, height:100},
+  8: {x:0, y:700, width:100, height:100},
+  9: {x:0, y:800, width:100, height:50},
+  10: {x:0, y:850, width:100, height:50},
+  11: {x:0, y:900, width:100, height:50},
 
-  12: [0, 950, 100, 100],
-  13: [0, 1050, 100, 100],
-  14: [0, 1150, 100, 100],
-  15: [0, 1250, 100, 100],
-  16: [0, 1350, 100, 100],
-  17: [0, 1450, 100, 100],
-  18: [0, 1550, 100, 100],
-  19: [0, 1650, 100, 100],
-  20: [0, 1750, 100, 50],
-  21: [0, 1850, 100, 50],
-  22: [0, 1950, 100, 50]
+  12: {x:0, y:950, width:100, height:100},
+  13: {x:0, y:1050, width:100, height:100},
+  14: {x:0, y:1150, width:100, height:100},
+  15: {x:0, y:1250, width:100, height:100},
+  16: {x:0, y:1350, width:100, height:100},
+  17: {x:0, y:1450, width:100, height:100},
+  18: {x:0, y:1550, width:100, height:100},
+  19: {x:0, y:1650, width:100, height:100},
+  20: {x:0, y:1750, width:100, height:50},
+  21: {x:0, y:1850, width:100, height:50},
+  22: {x:0, y:1950, width:100, height:50}
 
 }
+
 var decorationShapes = {
-  1: [0, 0, 100, 100]
+  1: {x:0, y:0, width:100, height:100}
 }
 
-var x=0;
-var y=0;
 var largeBlockSize = 20;
 var smallBlockSize = 10;
 
 var mountain = [
-  {'block':4, pos:[x,y+130,largeBlockSize,largeBlockSize]},
-  {'block':4, pos:[x+20,y+130,largeBlockSize,largeBlockSize]},
-  {'block':4, pos:[x+40,y+130,largeBlockSize,largeBlockSize]},
-  {'block':4, pos:[x+60,y+130,largeBlockSize,largeBlockSize]},
-  {'block':13, pos:[x+80,y+130,largeBlockSize,largeBlockSize]},
-  {'block':14, pos:[x+100,y+130,largeBlockSize,largeBlockSize]},
-  {'block':1, pos:[x+120,y+130,largeBlockSize,largeBlockSize]},
-  {'block':1, pos:[x+140,y+130,largeBlockSize,largeBlockSize]},
-  {'block':1, pos:[x+160,y+130,largeBlockSize,largeBlockSize]},
-  {'block':1, pos:[x+180,y+130,largeBlockSize,largeBlockSize]},
-  {'block':15, pos:[x+200,y+130,largeBlockSize,largeBlockSize]},
-  {'block':15, pos:[x+220,y+130,largeBlockSize,largeBlockSize]},
-  {'block':15, pos:[x+240,y+130,largeBlockSize,largeBlockSize]},
-  {'block':1, pos:[x+260,y+130,largeBlockSize,largeBlockSize]},
-  {'block':1, pos:[x+280,y+130,largeBlockSize,largeBlockSize]},
+  {'block':4, pos:{x:0,y:130,width:largeBlockSize,height:largeBlockSize}},
+  {'block':4, pos:{x:20,y:130,width:largeBlockSize,height:largeBlockSize}},
+  {'block':4, pos:{x:40,y:130,width:largeBlockSize,height:largeBlockSize}},
+  {'block':4, pos:{x:60,y:130,width:largeBlockSize,height:largeBlockSize}},
+  {'block':13, pos:{x:80,y:130,width:largeBlockSize,height:largeBlockSize}},
+  {'block':14, pos:{x:100,y:130,width:largeBlockSize,height:largeBlockSize}},
+  {'block':1, pos:{x:120,y:130,width:largeBlockSize,height:largeBlockSize}},
+  {'block':1, pos:{x:140,y:130,width:largeBlockSize,height:largeBlockSize}},
+  {'block':1, pos:{x:160,y:130,width:largeBlockSize,height:largeBlockSize}},
+  {'block':1, pos:{x:180,y:130,width:largeBlockSize,height:largeBlockSize}},
+  {'block':15, pos:{x:200,y:130,width:largeBlockSize,height:largeBlockSize}},
+  {'block':15, pos:{x:220,y:130,width:largeBlockSize,height:largeBlockSize}},
+  {'block':15, pos:{x:240,y:130,width:largeBlockSize,height:largeBlockSize}},
+  {'block':1, pos:{x:260,y:130,width:largeBlockSize,height:largeBlockSize}},
+  {'block':1, pos:{x:280,y:130,width:largeBlockSize,height:largeBlockSize}},
 
-  {'block':3, pos:[x+120,y+110,largeBlockSize,largeBlockSize]},
-  {'block':4, pos:[x+140,y+110,largeBlockSize,largeBlockSize]},
-  {'block':4, pos:[x+160,y+110,largeBlockSize,largeBlockSize]},
-  {'block':1, pos:[x+180,y+110,largeBlockSize,largeBlockSize]},
-  {'block':18, pos:[x+200,y+110,largeBlockSize,largeBlockSize]},
-  {'block':12, pos:[x+220,y+110,largeBlockSize,largeBlockSize]},
-  {'block':17, pos:[x+240,y+110,largeBlockSize,largeBlockSize]},
-  {'block':1, pos:[x+260,y+110,largeBlockSize,largeBlockSize]},
-  {'block':2, pos:[x+280,y+110,largeBlockSize,largeBlockSize]},
+  {'block':3, pos:{x:120,y:110,width:largeBlockSize,height:largeBlockSize}},
+  {'block':4, pos:{x:140,y:110,width:largeBlockSize,height:largeBlockSize}},
+  {'block':4, pos:{x:160,y:110,width:largeBlockSize,height:largeBlockSize}},
+  {'block':1, pos:{x:180,y:110,width:largeBlockSize,height:largeBlockSize}},
+  {'block':18, pos:{x:200,y:110,width:largeBlockSize,height:largeBlockSize}},
+  {'block':12, pos:{x:220,y:110,width:largeBlockSize,height:largeBlockSize}},
+  {'block':17, pos:{x:240,y:110,width:largeBlockSize,height:largeBlockSize}},
+  {'block':1, pos:{x:260,y:110,width:largeBlockSize,height:largeBlockSize}},
+  {'block':2, pos:{x:280,y:110,width:largeBlockSize,height:largeBlockSize}},
 
-  {'block':5, pos:[x+180,y+90,largeBlockSize,largeBlockSize]},
-  {'block':1, pos:[x+200,y+90,largeBlockSize,largeBlockSize]},
-  {'block':20, pos:[x+220,y+90,largeBlockSize,largeBlockSize]},
-  {'block':21, pos:[x+240,y+90,largeBlockSize,largeBlockSize]},
-  {'block':6, pos:[x+260,y+90,largeBlockSize,largeBlockSize]},
+  {'block':5, pos:{x:180,y:90,width:largeBlockSize,height:largeBlockSize}},
+  {'block':1, pos:{x:200,y:90,width:largeBlockSize,height:largeBlockSize}},
+  {'block':20, pos:{x:220,y:90,width:largeBlockSize,height:largeBlockSize}},
+  {'block':21, pos:{x:240,y:90,width:largeBlockSize,height:largeBlockSize}},
+  {'block':6, pos:{x:260,y:90,width:largeBlockSize,height:largeBlockSize}},
 
-  {'block':7, pos:[x+200,y+70,largeBlockSize,largeBlockSize]},
-  {'block':1, pos:[x+220,y+70,largeBlockSize,largeBlockSize]},
-  {'block':8, pos:[x+240,y+70,largeBlockSize,largeBlockSize]},
+  {'block':7, pos:{x:200,y:70,width:largeBlockSize,height:largeBlockSize}},
+  {'block':1, pos:{x:220,y:70,width:largeBlockSize,height:largeBlockSize}},
+  {'block':8, pos:{x:240,y:70,width:largeBlockSize,height:largeBlockSize}},
 
-  {'block':9, pos:[x+200,60,largeBlockSize,smallBlockSize]},
-  {'block':11, pos:[x+220,60,largeBlockSize,smallBlockSize]},
-  {'block':10, pos:[x+240,60,largeBlockSize,smallBlockSize]}
+  {'block':9, pos:{x:200,y:60,width:largeBlockSize,height:smallBlockSize}},
+  {'block':11, pos:{x:220,y:60,width:largeBlockSize,height:smallBlockSize}},
+  {'block':10, pos:{x:240,y:60,width:largeBlockSize,height:smallBlockSize}}
 ]
-var backgroundXLoc = 0;
-var canvasXLoc = 0;
 
 // everything in here happens once the HTML document is loaded.
 $(function() {
@@ -177,7 +179,7 @@ function initializeGraphics(){
   dude.image = new Image();
   dude.image.onload = loadedImage;
   mainCharacter = new GameCharacter();
-  mainCharacter.init(Math.round(playfield.width/3), 95, dude, playfield, context);
+  mainCharacter.init(20, 95, dude, playfield, context);
   dude.image.src = "images/Mario.png";
 
 }
@@ -205,7 +207,7 @@ function gameStart() {
         return;
     $('#playfield').css('background-position', '0px 0');
     gameStarted = true;
-    mainCharacter.init(Math.round(playfield.width/3), 95, dude, playfield, context);
+    mainCharacter.init(20, 95, dude, playfield, context);
     mainCharacter.draw();
     level1.init(0,0,playfield, context);
     level1.draw();
@@ -228,16 +230,9 @@ function mainLoop() {
 }
 
 function update(){
-  for (idx=0;idx<mountain.length;idx++){
-    var blockPos = mountain[idx].pos;
-    var block = {x:blockPos[0],y:blockPos[1],width:(blockPos[2]-blockPos[0]),height:blockPos[3]-blockPos[1]};
-    isCollide(block,mainCharacter.bounds());
-    console.log(`collide:`+isCollide(block,mainCharacter.bounds()));
-  }
-}
-
-function isCollide(a, b) {
-
+  mainCharacter.update();
+  level1.update();
+  level1.collide(mainCharacter);
 }
 
 function draw(){
@@ -245,7 +240,6 @@ function draw(){
   mainCharacter.draw();
 }
 
-var backgroundTimer;
 function moveBackground() {
   var x = 0;
   setInterval(function(){
@@ -330,10 +324,38 @@ function GameCharacter() {
     this.moveIndex = 0;
     this.jumpIndex = -1;
     this.jumpArc = [20,40,60,80,60,40,20];
+    this.collideRight = false;
+    this.collideLeft = false;
 	}
 
   this.bounds = function(){
-    return {x:this.x, y:this.y, width:this.currentPose.width, height:this.currentPose.height};
+    return {x:this.x, y:this.y - this.height, width:this.currentPose.width/3, height:this.currentPose.height/3};
+  }
+
+  this.center = function(){
+    return {x:this.x+((this.currentPose.width/3)/2), y:this.y+((this.currentPose.height/3)/2)};
+  }
+
+  this.update = function(){
+    this.collideLeft = false;
+    this.collideRight = false;
+  }
+
+  var blockLeft;
+
+  this.collide = function(displayBlock, distanceBetweenCenters){
+    console.log('bump');
+    var blockCenter = displayBlock.center();
+    var characterCenter = this.center;
+
+    if (distanceBetweenCenters>0){
+      this.collideLeft = true;
+    }
+
+    if (distanceBetweenCenters<0){
+      this.collideRight = true;
+    }
+
   }
 
   this.draw = function(){
@@ -345,10 +367,10 @@ function GameCharacter() {
   this.speed = function(speed){
     this.moving = true;
     this.context.clearRect(this.x,this.y - this.height,this.currentPose.width,this.currentPose.height );
-    if (speed<0 && this.x>=0){
+    if (speed<0 && this.x>=0 && !this.collideLeft){
       this.x += speed;
       this.currentPose = this.character.runningRight[this.moveIndex % (this.character.runningRight.length)];
-    } else if (speed>0 && (this.x+this.currentPose.width)<=this.canvas.width) {
+    } else if (speed>0 && (this.x+this.currentPose.width)<=this.canvas.width  && !this.collideRight) {
       this.x += speed;
       this.currentPose = this.character.runningRight[this.moveIndex % (this.character.runningRight.length)];
     } else {
@@ -376,22 +398,95 @@ function GamePlatform1() {
     this.context = context;
     this.canvas = canvas;
     this.moving = false;
+    this.blockArray = [];
+    for (idx=0;idx<mountain.length;idx++){
+      var imgMapCoords = groundImageMap[mountain[idx].block];
+      var blockPos = mountain[idx].pos;
+      var blockObj = new GameBlock();
+      blockObj.init(imgMapCoords.x,imgMapCoords.y,imgMapCoords.width,imgMapCoords.height,
+        blockPos.x,blockPos.y,blockPos.width,blockPos.height,
+        canvas, context);
+      this.blockArray.push(blockObj)
+    }
+  }
+
+  this.collide = function(character){
+    for (i = 0; i < this.blockArray.length; i++) {
+      var gameBlock = this.blockArray[i];
+      var yRow = gameBlock.y+gameBlock.height;
+      var characterRow = character.bounds().y+character.bounds().height;
+      if (characterRow<=yRow && characterRow>gameBlock.y){
+        gameBlock.collide(character);
+      }
+    }
+  }
+
+  this.update = function(){
+
   }
 
   this.draw = function(){
-    for (idx=0;idx<mountain.length;idx++){
-      var blockCoords = groundShapes[mountain[idx].block];
-      var blockPos = mountain[idx].pos;
-      context.drawImage(images.groundImage,
-        this.x+blockCoords[0],this.y+blockCoords[1],blockCoords[2],blockCoords[3],
-        blockPos[0],blockPos[1],blockPos[2],blockPos[3]);
+    for (i = 0; i < this.blockArray.length; i++) {
+      var gameBlock = this.blockArray[i];
+      gameBlock.draw();
     }
   }
 
   this.remove = function(xPos){
     this.moving = true;
-    this.context.clearRect(0,0,this.canvas.width,this.canvas.height );
-    this.x-=xPos;
+    for (i = 0; i < this.blockArray.length; i++) {
+      var gameBlock = this.blockArray[i];
+      gameBlock.move(xPos);
+    }
+    this.context.clearRect(0,0,this.canvas.width,this.canvas.height);
+  }
+}
+
+function GameBlock() {
+	this.init = function(imgMapX, imgMapY, imgMapWidth, imgMapHeight, x, y, width, height, canvas, context) {
+    // Default variables
+    this.imgMapX = imgMapX;
+    this.imgMapY = imgMapY
+    this.imgMapWidth = imgMapWidth;
+    this.imgMapHeight = imgMapHeight;
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.context = context;
+    this.canvas = canvas;
+    this.sameRow = false;
+  }
+
+  this.center = function(){
+    return {x:this.x+(this.width/2) , y:this.y+(this.height/2)};
+  }
+
+  this.collide = function(object){
+    this.sameRow = true;
+    var objBounds = object.bounds();
+    var objCenter = object.center();
+    var distanceBetweenCenters = objCenter.x - this.center().x ;
+    if (Math.abs(distanceBetweenCenters)<( (objBounds.width/2)+(this.width/2) ) ) {
+      object.collide(this, distanceBetweenCenters);
+    }
+  }
+
+  this.draw = function(){
+    this.context.drawImage(images.groundImage,
+      this.imgMapX,
+      this.imgMapY,
+      this.imgMapWidth,
+      this.imgMapHeight,
+      this.x,
+      this.y,
+      this.width,
+      this.height);
+  }
+
+  this.move = function(xPos){
+    this.context.clearRect(this.x,this.y,this.width,this.height );
+    this.x=xPos;
     this.draw();
   }
 }
