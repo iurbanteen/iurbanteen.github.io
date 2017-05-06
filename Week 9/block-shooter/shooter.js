5835ae4ce4b1bec114a45f302d35448e027ae8ee
camerono <camough@gmail.com>
2017-05-06 11:54:57 -0700

    added firing to hero

--- Week 9/block-shooter/shooter.js
+++ Week 9/block-shooter/shooter.js
@@ -11,20 +11,27 @@ $(function() {
     playfield = document.getElementById('playfield');
     context = playfield.getContext("2d");
     // initialization routine(s)
+    initializeListeners();
     enemy.removeAndReplaceOffscreenShips();
     enemy.drawShips();
+    hero.initialize();
+    hero.draw();
 });
 
 
 var timer;
 var countdownSeconds = 10;
+var counter;
 function startTimer(){
-  gameLoop = setInterval(function() {
-    countdownSeconds--;
-    if (countdownSeconds == 0){
+  counter = countdownSeconds;
+  updateClock(counter);
+  timer = setInterval(function() {
+    --counter;
+    if (counter == 0){
       stopTimer();
       gameEnd();
     }
+    updateClock(counter);
   }, 1000);
 }
 function stopTimer(){
@@ -32,12 +39,16 @@ function stopTimer(){
 }
 
 var gameLoop;
-var ticks = 0;
+
 function gameStart(){
   startTimer();
   gameLoop = setInterval(function() {
     gameTick();
   }, 1);
+}
+// update the onscreen timer
+function updateClock(secondsRemaining) {
+    $('#timer').text(secondsRemaining);
 }
 
 function gameTick(){
@@ -47,12 +58,86 @@ function gameTick(){
   enemy.setOffscreenShips();
   enemy.removeAndReplaceOffscreenShips();
   enemy.drawShips();
-  //if (++ticks > 1000) gameEnd();
+  hero.draw();
 }
 
 function gameEnd(){
   clearInterval(gameLoop);
   //ticks = 0;
+}
+var hero = {
+  x,
+  y : 500,
+  width : 60,
+  height : 30,
+  hit : false,
+  speed : 1,
+  direction : 0,
+  moving : false,
+  hit : false,
+  bulletLimit : 5,
+  bullets : [],
+  draw(){
+    context.clearRect(this.x-2, this.y-2, this.width+4, this.height+4);
+    // Determine if the action is move action
+		if (KEY_STATUS.left || KEY_STATUS.right) {
+			if (KEY_STATUS.left) {
+				this.x -= this.speed
+				if (this.x <= 0) // Keep player within the screen
+					this.x = 0;
+			} else if (KEY_STATUS.right) {
+				this.x += this.speed
+				if (this.x >= this.canvasWidth - this.width)
+					this.x = this.canvasWidth - this.width;
+			}
+    }
+    context.translate(this.x, this.y);
+
+    context.beginPath();
+    context.ellipse(0, 0, this.width, this.height, 0, 0, 2 * Math.PI);
+    context.fillStyle = 'lightblue';
+    context.fill();
+    context.lineWidth = 2;
+    context.strokeStyle = 'darkblue';
+    context.stroke();
+    context.closePath();
+    // fire!!!
+    if (KEY_STATUS.space) {
+      this.fire();
+    }
+    //context.fillRect(this.);
+    // reset current transformation matrix to the identity matrix
+    context.setTransform(1, 0, 0, 1, 0, 0);
+    this.drawBullets();
+  },
+  initialize(){
+    this.x = Math.round( (playfield.width + this.width) / 2);
+  },
+  fire(){
+    if(this.bullets.length < this.bulletLimit){
+      this.bullets.push({x:this.x, y: this.y});
+    }
+  },
+  drawBullets(){
+    // remove offscreen bullets
+    for(var i = this.bullets.length; i--; ){
+      if (this.bullets[i].y < 0) this.bullets.splice(i, 1);
+    }
+    for(var i = this.bullets.length; i--; ){
+      //console.log(`bullet at ${i} : ${}`)
+      this.drawBullet(this.bullets[i]);
+    }
+  },
+  drawBullet(bullet){
+    bullet.y -= 5;
+    context.beginPath();
+    context.moveTo(bullet.x, bullet.y);
+    context.lineTo(bullet.x, bullet.y - 5);
+    context.lineWidth = 4;
+    context.strokeStyle = 'darkred';
+    context.stroke();
+    context.closePath();
+  }
 }
 
 var enemy = {
@@ -168,4 +253,43 @@ var enemy = {
     }
   }
 
+}
+
+// register the key press listener
+function initializeListeners() {
+    document.onkeydown = keyDownEventHandler;
+    document.onkeyup = keyUpEventHandler;
+    // will need to add 'key up' for continual movement
+}
+
+// Translate key press event into direction
+function keyDownEventHandler(event) {
+   if (KEY_CODES[event.keyCode]) {
+     event.preventDefault();
+     KEY_STATUS[KEY_CODES[event.keyCode]] = true;
+   }
+}
+function keyUpEventHandler(event) {
+  if (KEY_CODES[event.keyCode]) {
+    event.preventDefault();
+    KEY_STATUS[KEY_CODES[event.keyCode]] = false;
+  }
+}
+
+// The keycodes that will be mapped when a user presses a button.
+// Original code by Doug McInnes
+KEY_CODES = {
+  32: 'space',
+  37: 'left',
+  38: 'up',
+  39: 'right',
+  40: 'down',
+}
+// Creates the array to hold the KEY_CODES and sets all their values
+// to false. Checking true/flase is the quickest way to check status
+// of a key press and which one was pressed when determining
+// when to move and which direction.
+KEY_STATUS = {};
+for (code in KEY_CODES) {
+  KEY_STATUS[ KEY_CODES[ code ]] = false;
 }
